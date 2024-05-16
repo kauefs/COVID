e1145ae2c19f18326e9d046b4b62f7a116da62f7
KAUE <139394184+kauefs@users.noreply.github.com>
2024-05-16 16:27:23 -0300

    COVID19BR.py

--- COVID19BR.py
+++ COVID19BR.py
@@ -36,14 +36,7 @@ nan      = X.select_dtypes(exclude=['number']).columns
 X[nan]   = X[nan].fillna('N/A')
 OWID     = X.copy()
 # SIDE:
-st.sidebar.title(   'ƊⱭȾɅViƧi🧿Ƞ')
-st.sidebar.markdown('''
-[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
-[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
-[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
-[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
-[![License]( https://img.shields.io/github/license/kauefs/COVID?logo=mit&logoColor=FFFFFF&label=License&labelColor=8B959E&color=750014)](https://web.mit.edu/Saltzer/www/publications/MITLicense.pdf)
-                    ''')
+st.sidebar.title(    'ƊⱭȾɅViƧi🧿Ƞ'      )
 st.sidebar.divider(                      )
 st.sidebar.header(   'COVID-19 in Brazil')
 st.sidebar.subheader('Data Analysis'     )
@@ -52,7 +45,14 @@ st.sidebar.markdown('''Source:    [Our World in Data](https://covid.ourworldinda
 st.sidebar.write(    'OWID daily reports from {} to {}'.format(RAW.index.min(), RAW.index.max()))
 st.sidebar.markdown('''Reference: [Data Cleaning Techniques in Python: the Ultimate Guide](https://www.justintodata.com/data-cleaning-techniques-python-guide/)''')
 st.sidebar.divider(                      )
-st.sidebar.markdown('''2023.11.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
+st.sidebar.markdown('''
+[![GitHub](  https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](                                 https://github.com/kauefs/)
+[![Medium](  https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](                                 https://medium.com/@kauefs)
+[![LinkedIn](https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](                               https://www.linkedin.com/in/kauefs/)
+[![Python](  https://img.shields.io/badge/-3-4584B6?logo=python&logoColor=FFDE57&labelColor=4584B6&color=646464)](https://www.python.org/)
+[![License]( https://img.shields.io/github/license/kauefs/COVID?logo=mit&logoColor=FFFFFF&label=License&labelColor=8B959E&color=750014)](https://web.mit.edu/Saltzer/www/publications/MITLicense.pdf)
+
+2023.11.23 &copy; 2024 [ƊⱭȾɅViƧi🧿Ƞ](https://datavision.one/) &trade;''')
 # MAIN:
 st.divider(                     )
 st.title(   'COVID-19 in Brazil')
