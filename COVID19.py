000c6c30e0595cb32ffd8918e595f9a5dbd0c4e2
KAUE <139394184+kauefs@users.noreply.github.com>
2024-12-05 02:59:39 -0300

    COVID19.py

--- COVID19.py
+++ COVID19.py
@@ -58,7 +58,7 @@ st.sidebar.markdown('''
 
 [![GitHub](     https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
 [![Medium](     https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
-[![LinkedIn](   https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![LinkedIn](   https://img.shields.io/badge/-2867B2?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
 [![Python](     https://img.shields.io/badge/-3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
 
 [![License](    https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
