91ecabd307e5c13c0b5e2909a42d384d6799b036
KAUE <139394184+kauefs@users.noreply.github.com>
2025-01-22 12:21:27 -0300

    COVID19.py

--- COVID19.py
+++ COVID19.py
@@ -1,13 +1,13 @@
 # Libraries:
-import numpy               as np
-import pandas              as pd
-import matplotlib.pyplot   as plt
-import matplotlib.gridspec as gs
-import matplotlib.ticker   as ticker
-import matplotlib.dates    as mdates
-import seaborn             as sns
-import streamlit           as st
-from   datetime        import date, datetime, timedelta
+import streamlit           as   st
+import numpy               as   np
+import pandas              as   pd
+import matplotlib.gridspec as   gs
+import matplotlib.pyplot   as   plt
+import matplotlib.ticker   as   ticker
+import matplotlib.dates    as   mdates
+import seaborn             as   sns
+from   datetime          import date, datetime, timedelta
 st.set_page_config(page_title='COVID19', page_icon='😷', layout='wide', initial_sidebar_state='expanded')
 # DATA:
 DATA     = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
@@ -27,7 +27,7 @@ def LoadData():
     return X
 df      = LoadData()
 # SIDE:
-st.sidebar.title(    'ƊⱭȾɅViƧi🧿Ƞ'      )
+st.sidebar.title(    'ƊⱭȾɅViƧi🧿Ƞ&trade;'      )
 st.sidebar.divider(                      )
 st.sidebar.title(    'COVID-19'          )
 st.sidebar.header(   'Data Analysis'     )
@@ -58,12 +58,12 @@ st.sidebar.markdown('''
 
 [![GitHub](     https://img.shields.io/badge/-000000?logo=github&logoColor=FFFFFF)](https://github.com/kauefs/)
 [![Medium](     https://img.shields.io/badge/-000000?logo=medium&logoColor=FFFFFF)](https://medium.com/@kauefs)
-[![LinkedIn](   https://img.shields.io/badge/-2867B2?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
-[![Python](     https://img.shields.io/badge/-3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
+[![LinkedIn](   https://img.shields.io/badge/-0077B5?logo=linkedin&logoColor=FFFFFF)](https://www.linkedin.com/in/kauefs/)
+[![Python](     https://img.shields.io/badge/3-646464?logo=python&logoColor=FFDE57&labelColor=4584B6)](https://www.python.org/)
 
-[![License](    https://img.shields.io/badge/Apache--2.0-D22128?style=flat&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
+[![License](    https://img.shields.io/badge/Apache--2.0-D22128?&logo=apache&logoColor=CB2138&label=License&labelColor=6D6E71)](https://www.apache.org/licenses/LICENSE-2.0)
 
-[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&label=&copy;2023&labelColor=0065FF)](https://datavision.one/)
+[![ƊⱭȾɅViƧi🧿Ƞ](https://img.shields.io/badge/ƊⱭȾɅViƧi🧿Ƞ&trade;-0065FF?style=plastic&logoColor=0065FF&label=&copy;2023&labelColor=0065FF)](https://datavision.one/)
                     ''')
 # MAIN:
 st.divider(            )
